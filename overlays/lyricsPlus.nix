2d91cfede3e4772debd691fb4ae3226762e93217
hexpilgrim <hexpilgrim@proton.me>
2025-08-14 06:21:47 +0100

    chore(overlays): update lyricsPlus

--- overlays/lyricsPlus.nix
+++ overlays/lyricsPlus.nix
@@ -7,7 +7,7 @@ system: self: super: {
       owner = "spicetify";
       repo = "cli";
       rev = "702e027fde6edf2ccd30ef97cb92a44f42a95c6c";
-      sha256 = "9rX3jifEmGL1ewiTu3DoUYI/u8Osl1wVSo+9yWH+J8I=";
+      sha256 = "GkJjqas+DTh8jJhOyY6RtxkkYs1CHH1ngD8kahpbD4k=";
     };
 
     installPhase = ''
